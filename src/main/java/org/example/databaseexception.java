4e762e5b0da26ab2256121775adb3c5effa89c5f
bhuvaneshwari-k <123533635+bhuvaneshwari-k@users.noreply.github.com>
2023-01-31 14:41:42 +0530

    Update databaseexception.java

--- src/main/java/org/example/databaseexception.java
+++ src/main/java/org/example/databaseexception.java
@@ -1,36 +1,36 @@
 package org.example;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Scanner;
- class database {
-    static database db;
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Logger;
+class Database {
+    Logger l=Logger.getLogger("com.api.jar");
+    static Database db;
     Connection conn;
-    private database(){}
-    public static database getdatabase(){
+    private  Database(){}
+    public static Database getdatabase(){
         if(db==null)
-            db=new database();
+            db=new Database();
         return db;
     }
     void newConnection(String url1,String user1,String pass1) throws SQLException{
         conn= DriverManager.getConnection(url1,user1,pass1);
-        System.out.println("Connection created");
+        l.info("Connection created");
     }
     void closeConnection() throws SQLException{
         conn.close();
-        System.out.println("Connection destroyed");
+        l.info("Connection destroyed");
     }
     public static void main(String[] args) throws SQLException {
         Scanner s=new Scanner(System.in);
-        System.out.println("Enter the url:");
-    String url=s.nextLine();
-        System.out.println("Enter the username:");
-    String user=s.nextLine();
-        System.out.println("Enter the password:");
-    String pass=s.nextLine();
-    database d1=database.getdatabase();
-    d1.newConnection(url,user,pass);
-    d1.closeConnection();
+        Logger l=Logger.getLogger("com.api.jar");
+        l.info("Enter the url:");
+        String url=s.nextLine();
+        l.info("Enter the username:");
+        String user=s.nextLine();
+        l.info("Enter the password:");
+        String pass=s.nextLine();
+        Database d1=Database.getdatabase();
+        d1.newConnection(url,user,pass);
+        d1.closeConnection();
     }
 }
